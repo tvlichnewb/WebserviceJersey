5c712d8a715c99348f98ef96ed2906dd5528ba0b
tvlictnewb <tvlictnewb@gmail.com>
2015-08-11 23:13:39 +0700

    First vs

---
+++ src/main/java/lichtv/com/dao/impl/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @create date : Aug 11, 2015
+ * @copyright Lich Truong Van - lichtv
+ * @author lichtv - Software Engineer / Computer Science
+ * @contact : 1/Email : lichcse@gmail.com  2/Skype : lichcse  3/Mobile : (+84)1677897159
+ */
+/**
+ * Description File.
+ * 
+ */
+package lichtv.com.dao.impl;
